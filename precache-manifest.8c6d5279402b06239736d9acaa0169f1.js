125080e75c8d03a6b45490704cbef981b71a04dd
jaaybe <jb@motionmusic.us>
2020-10-04 14:45:39 -0700

    Updates

---
+++ precache-manifest.8c6d5279402b06239736d9acaa0169f1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e33cce69df6b675dec6555401472b81",
+    "url": "//jaaybe.github.io/challenge20_react/index.html"
+  },
+  {
+    "revision": "55405a91dcd3f04fc341",
+    "url": "//jaaybe.github.io/challenge20_react/static/css/main.56dad6b5.chunk.css"
+  },
+  {
+    "revision": "33258118986563be078d",
+    "url": "//jaaybe.github.io/challenge20_react/static/js/2.615088d2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "//jaaybe.github.io/challenge20_react/static/js/2.615088d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55405a91dcd3f04fc341",
+    "url": "//jaaybe.github.io/challenge20_react/static/js/main.29218867.chunk.js"
+  },
+  {
+    "revision": "8f590835354be3a9fcff",
+    "url": "//jaaybe.github.io/challenge20_react/static/js/runtime-main.dca005c8.js"
+  },
+  {
+    "revision": "10c56cfe528c8b824b09c5b288489e73",
+    "url": "//jaaybe.github.io/challenge20_react/static/media/background.10c56cfe.png"
+  },
+  {
+    "revision": "10b6299fd5b962e9a4c837e7ef84862e",
+    "url": "//jaaybe.github.io/challenge20_react/static/media/jbResume.10b6299f.pdf"
+  },
+  {
+    "revision": "625fd02e39d04204ab914daa9abdcf92",
+    "url": "//jaaybe.github.io/challenge20_react/static/media/jm_korea.625fd02e.jpg"
+  },
+  {
+    "revision": "35966dffe66ffe3681f2d0b6894a6e41",
+    "url": "//jaaybe.github.io/challenge20_react/static/media/portrait.35966dff.png"
+  }
+]);
