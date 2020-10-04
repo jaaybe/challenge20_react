0b5a0acba9f21898e43b29d4dca9f5f2921bd361
jaaybe <jb@motionmusic.us>
2020-10-04 14:52:46 -0700

    Updates

---
+++ precache-manifest.95f512504bc46dd00261aff0aa06c0d3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c92bcfe7edd2db17f0a9ff5eee329199",
+    "url": "/challenge20_react/index.html"
+  },
+  {
+    "revision": "24bd852010e896477034",
+    "url": "/challenge20_react/static/css/main.56dad6b5.chunk.css"
+  },
+  {
+    "revision": "33258118986563be078d",
+    "url": "/challenge20_react/static/js/2.615088d2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/challenge20_react/static/js/2.615088d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24bd852010e896477034",
+    "url": "/challenge20_react/static/js/main.df811f4e.chunk.js"
+  },
+  {
+    "revision": "3f1b214fb809d37d231b",
+    "url": "/challenge20_react/static/js/runtime-main.a133e703.js"
+  },
+  {
+    "revision": "10c56cfe528c8b824b09c5b288489e73",
+    "url": "/challenge20_react/static/media/background.10c56cfe.png"
+  },
+  {
+    "revision": "10b6299fd5b962e9a4c837e7ef84862e",
+    "url": "/challenge20_react/static/media/jbResume.10b6299f.pdf"
+  },
+  {
+    "revision": "625fd02e39d04204ab914daa9abdcf92",
+    "url": "/challenge20_react/static/media/jm_korea.625fd02e.jpg"
+  },
+  {
+    "revision": "35966dffe66ffe3681f2d0b6894a6e41",
+    "url": "/challenge20_react/static/media/portrait.35966dff.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "//jaaybe.github.io/challenge20_react/precache-manifest.8c6d5279402b06239736d9acaa0169f1.js"
+  "/challenge20_react/precache-manifest.95f512504bc46dd00261aff0aa06c0d3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//jaaybe.github.io/challenge20_react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/challenge20_react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
